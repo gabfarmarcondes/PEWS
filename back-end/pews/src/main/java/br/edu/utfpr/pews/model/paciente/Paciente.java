b8a93906a6fe5c0a5d6fa5a47a6cf2b2a4e8a7fb
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2024-11-11 01:32:56 -0300

    Criando o PacienteDTO e Calculando o Total de DIH do Paciente

--- back-end/pews/src/main/java/br/edu/utfpr/pews/model/paciente/Paciente.java
+++ back-end/pews/src/main/java/br/edu/utfpr/pews/model/paciente/Paciente.java
@@ -1,14 +1,20 @@
 package br.edu.utfpr.pews.model.paciente;
 
+import br.edu.utfpr.pews.dto.PacienteDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paciente {
 
     @Id
@@ -21,4 +27,24 @@ public class Paciente {
 
     private LocalDateTime  DIHentrada;
     private LocalDateTime DIHalta;
+    private long DIHtotal;
+
+    public long getDIHtotal(LocalDateTime DIHalta, LocalDateTime DIHentrada) {
+        this.DIHalta = DIHalta;
+        this.DIHentrada = DIHentrada;
+        if (DIHalta != null && DIHentrada != null) {
+            return this.DIHtotal = Duration.between(DIHalta, DIHentrada).toDays();
+        }
+        return 0;
+    }
+
+    public Paciente(PacienteDTO pacienteDTO){
+        this.nome = pacienteDTO.nome();
+        this.anoNascimento = pacienteDTO.anoNascimento();
+        this.diagnostico = pacienteDTO.diagnostico();
+        this.leito = pacienteDTO.leito();
+        this.DIHentrada = pacienteDTO.DIHentrada();
+        this.DIHalta = pacienteDTO.DIHalta();
+        this.DIHtotal = getDIHtotal(this.DIHentrada, this.DIHalta);
+    }
 }
